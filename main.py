219b5376e73d2e7250730647f626d17daa30602f
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-10-23 23:26:03 +0200

    first change

---
+++ main.py
@@ -0,0 +1,14 @@
+#ejercicio1
+import math
+radio = int(input("introduzca el radio: "))
+area = (radio ^ 2) * math.pi
+print(area)
+
+#ejercicio2
+def lee_numero():
+    numero1 = int(input("introduzca un primer numero: "))
+    numero2 = int(input("introduzca un segundo numero: "))
+    numero3 = int(input("introduzca un tercer numero: "))
+return 
+
+  
